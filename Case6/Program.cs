6dfb538216010734651700506b14de0132d028cf
tarasbiakin <115660593+tarasbiakin@users.noreply.github.com>
2022-11-11 22:54:44 +0300

    Case6

---
+++ Case6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace HomeWork{
+    class Program{
+        static void Main(){
+            System.Console.WriteLine("Введите  число");
+            int a = Convert.ToInt16(Console.ReadLine());
+             
+            if (a %2== 0)
+            System.Console.WriteLine("Число четное");
+            else
+              Console.WriteLine("Число нечетное");
+        }
+ 
+    }
+}           
