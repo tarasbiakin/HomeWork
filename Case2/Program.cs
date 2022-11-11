fc8c0fc5565234a70084416e706b878d0f13c8e6
tarasbiakin <115660593+tarasbiakin@users.noreply.github.com>
2022-11-11 13:27:42 +0300

    Case2

---
+++ Case2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace HomeWork{
+    class Program{
+        static void Main(){
+            System.Console.WriteLine("Введите первое число");
+            int a = Convert.ToInt16(Console.ReadLine());
+            System.Console.WriteLine("Введите второе число");
+            int b = Convert.ToInt16(Console.ReadLine());
+            
+            
+            if (a > b)
+            {
+                System.Console.WriteLine("А больше Б");
+            }
+            else if (a < b)
+            {
+                Console.WriteLine("А меньше Б");
+            }
+            else
+            {
+                Console.WriteLine("Они равны");
+            }
+
+
+
+
+        }
+    }
+}
+
+
+
