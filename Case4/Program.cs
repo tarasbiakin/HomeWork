b18da416499e00800186e29ec3e4d38364ed61ca
tarasbiakin <115660593+tarasbiakin@users.noreply.github.com>
2022-11-11 17:43:42 +0300

    Case4

---
+++ Case4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace HomeWork{
+    class Program{
+        static void Main(){
+             System.Console.WriteLine("Введите первое число");
+            int a = Convert.ToInt16(Console.ReadLine());
+            System.Console.WriteLine("Введите второе число");
+            int b = Convert.ToInt16(Console.ReadLine());
+            System.Console.WriteLine("Введите третье число");
+            int c = Convert.ToInt16(Console.ReadLine());
+
+            int max = 0;
+            if(a >b){
+                max = a;
+                 System.Console.WriteLine("Наибольшее число  " + max); }
+                 else if(a<b){max = b;
+                 System.Console.WriteLine("Наибольшее число  " + max);
+                 }else if (c > max){
+                    System.Console.WriteLine("Наибольшее число  " + max);
+                 }
+
+            
+              
+      }
+    }
+}
+
+
