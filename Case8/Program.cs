15e68ea4ddb6b2ceb25862e6722caefc3b2567f2
tarasbiakin <115660593+tarasbiakin@users.noreply.github.com>
2022-11-12 18:25:11 +0300

    Case8

---
+++ Case8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace HomeWork{
+    class Program{
+        static void Main(){
+            
+            int[] nums;
+
+            System.Console.WriteLine("Введите  число");
+            int N = Convert.ToInt16(Console.ReadLine());
+            for (int i = 1; i <= N; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    System.Console.WriteLine(i + "Число четное");
+                }else
+                System.Console.WriteLine(i + "Число нечетное");
+            }
+           
+        }
+            
+    }
+}
+
